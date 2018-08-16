7b51a92aeed21fec386f4fb3545ebb257881c77c
Nathan Choung <nchou001@ucr.edu>
2018-08-15 20:55:01 -0700

    added knees from spreadsheet fixed pdac approval showing wrong list

--- ProstheticsApp/KneeListViewController.swift
+++ ProstheticsApp/KneeListViewController.swift
@@ -27,27 +27,48 @@ struct Knee {
 }
 
 let kneeList: [Knee] = [
-    Knee(serial: "VGK-125",name: "VGK Prosthetic Knee",pdacReq: 0,kLev: 3,weightLim: 275.0,buildHeight: 282.0),
-    Knee(serial: "M0703",name: "Dolphin Knee",pdacReq: 1,kLev: 3,weightLim: 220.0,buildHeight: 184.0),
-    Knee(serial: "M0780",name: "Swan 75 Knee",pdacReq: 1,kLev: 3,weightLim: 165.0,buildHeight: 172.0),
-    Knee(serial: "M0786",name: "Swan 100 Knee",pdacReq: 1,kLev: 3,weightLim: 275.0,buildHeight: 175.0),
-    Knee(serial: "1323",name: "Stance Flexion 5-Bar Pneumatic Knee",pdacReq: 1,kLev: 3,weightLim: 275.0,buildHeight: 219.0),
-    Knee(serial: "1324",name: "Stance Flexion 5-Bar Mechanical Knee",pdacReq: 1,kLev: 2,weightLim: 275.0,buildHeight: 160.0),
-    Knee(serial: "1341",name: "Safe Stride Knee",pdacReq: 0,kLev: 3,weightLim: 220.0,buildHeight: 157.77),
+    Knee(serial: "VGK-125P",name: "VGK Prosthetic Knee",pdacReq: 0,kLev: 3,weightLim: 275.0,buildHeight: 282.0),
+    Knee(serial: "VGK-125KD",name: "VGK Prosthetic Knee",pdacReq: 0,kLev: 3,weightLim: 275.0,buildHeight: 282.0),
+    Knee(serial: "2SR500",name: "Sensor-Hydraulic Swing and Stance Phase Control Knee",pdacReq: 0,kLev: 3,weightLim: 275.0,buildHeight: 246.0),
+    Knee(serial: "M0703",name: "Dolphin Knee",pdacReq: 1,kLev: 3,weightLim: 220.0,buildHeight: 142.0),
+    Knee(serial: "M0780",name: "Swan 75 Knee",pdacReq: 1,kLev: 3,weightLim: 165.0,buildHeight: 164.0),
+    Knee(serial: "M0786",name: "Swan 100 Knee",pdacReq: 1,kLev: 3,weightLim: 275.0,buildHeight: 170.0),
+    Knee(serial: "1323AP",name: "Stance Flexion 5-Bar Pneumatic Knee",pdacReq: 1,kLev: 3,weightLim: 275.0,buildHeight: 177.5),
+    Knee(serial: "1323AL",name: "Stance Flexion 5-Bar Pneumatic Knee",pdacReq: 1,kLev: 3,weightLim: 275.0,buildHeight: 183.8),
+    Knee(serial: "1322AP",name: "Polycentric Pneumatic 4-Bar Knee",pdacReq: 1,kLev: 3,weightLim: 275.0,buildHeight: 172.0),
+    Knee(serial: "1322AL",name: "Polycentric Pneumatic 4-Bar Knee",pdacReq: 1,kLev: 3,weightLim: 275.0,buildHeight: 178.3),
+    Knee(serial: "1341",name: "Safe Stride Knee",pdacReq: 0,kLev: 3,weightLim: 220.0,buildHeight: 156.6),
+    Knee(serial: "1320",name: "Pneumatic Single Axis Knee with Weight Activated Lock",pdacReq: 1,kLev: 3,weightLim: 220.0,buildHeight: 160.0),
+    Knee(serial: "1324AP",name: "Stance Flexion 5-Bar Mechanical Knee",pdacReq: 1,kLev: 2,weightLim: 275.0,buildHeight: 112.0),
+    Knee(serial: "1324AL",name: "Stance Flexion 5-Bar Mechanical Knee",pdacReq: 1,kLev: 2,weightLim: 275.0,buildHeight: 118.3),
+    
+    Knee(serial: "L1324AP",name: "Stance Flexion 5-Bar Mechanical Knee with Manual Lock",pdacReq: 1,kLev: 2,weightLim: 275.0,buildHeight: 112.0),
+    Knee(serial: "L1324AL",name: "Stance Flexion 5-Bar Mechanical Knee with Manual Lock",pdacReq: 1,kLev: 2,weightLim: 275.0,buildHeight: 118.3),
+    
+    Knee(serial: "1321AP",name: "Polycentric 4-Bar Knee",pdacReq: 1,kLev: 2,weightLim: 275.0,buildHeight: 108.0),
+    Knee(serial: "1321AL",name: "Polycentric 4-Bar Knee",pdacReq: 1,kLev: 2,weightLim: 275.0,buildHeight: 114.3),
+    
+    Knee(serial: "L1321AP",name: "Polycentric 4-Bar Manual Locking Knee",pdacReq: 1,kLev: 1,weightLim: 275.0,buildHeight: 108.0),
+    Knee(serial: "L1321AL",name: "Polycentric 4-Bar Manual Locking Knee",pdacReq: 1,kLev: 1,weightLim: 275.0,buildHeight: 114.3),
+    
     Knee(serial: "1319",name: "Pneumatic Single Axis Knee",pdacReq: 0,kLev: 3,weightLim: 220.0,buildHeight: 146.43),
-    Knee(serial: "1320",name: "Pneumatic Single Axis Knee with Weight Activated Lock",pdacReq: 1,kLev: 3,weightLim: 220.0,buildHeight: 159.68),
-    Knee(serial: "1322",name: "Polycentric Pneumatic 4-Bar Knee",pdacReq: 1,kLev: 3,weightLim: 275.0,buildHeight: 218.0),
-    Knee(serial: "1321",name: "Polycentric 4-Bar Knee",pdacReq: 1,kLev: 3,weightLim: 275.0,buildHeight: 160.0),
-    Knee(serial: "L1321",name: "Polycentric 4-Bar Manual Locking Knee",pdacReq: 1,kLev: 3,weightLim: 275.0,buildHeight: 160.0),
-    Knee(serial: "1326",name: "4-Bar Knee for AK and KD",pdacReq: 1,kLev: 3,weightLim: 220.0,buildHeight: 160.0),
-    Knee(serial: "1311",name: "Weight Activated Brake Knee",pdacReq: 1,kLev: 3,weightLim: 275.0,buildHeight: 42.44),
-    Knee(serial: "1313A",name: "Manual Locking Knee - Type A",pdacReq: 1,kLev: 3,weightLim: 198.0,buildHeight: 39.43),
-    Knee(serial: "1313A-N",name: "Manual Locking Knee - Type B",pdacReq: 1,kLev: 3,weightLim: 220.0,buildHeight: 39.43),
-    Knee(serial: "1312",name: "Mechanical 4-Bar Knee",pdacReq: 0,kLev: 3,weightLim: 275.0,buildHeight: 75.07),
-    Knee(serial: "1314",name: "4-Bar Knee with Extension Assist for KD",pdacReq: 1,kLev: 3,weightLim: 275.0,buildHeight: 111.45),
-    Knee(serial: "1315",name: "4-Bar Knee with Extension Assist for KD",pdacReq: 1,kLev: 3,weightLim: 275.0,buildHeight: 111.45),
-    Knee(serial: "1317",name: "Single Axis Knee with Internal Extension Assist and Adjustable Constant Friction",pdacReq: 1,kLev: 3,weightLim: 275.0,buildHeight: 77.32),
-    Knee(serial: "1318",name: "Single Axis Knee with Manual Lock",pdacReq: 1,kLev: 3,weightLim: 275.0,buildHeight: 77.32),
+    
+    Knee(serial: "1326AP",name: "4-Bar Knee for AK and KD",pdacReq: 1,kLev: 2,weightLim: 220.0,buildHeight: 87.3),
+    Knee(serial: "1326AL",name: "4-Bar Knee for AK and KD",pdacReq: 1,kLev: 2,weightLim: 220.0,buildHeight: 93.6),
+    
+
+    Knee(serial: "L1311A",name: "Weight Activated Brake Knee with Manual Lock",pdacReq: 1,kLev: 1,weightLim: 220.0,buildHeight: 65.0),
+    Knee(serial: "1311T",name: "Weight Activated Brake Knee",pdacReq: 1,kLev: 1,weightLim: 220.0,buildHeight: 48.0),
+    Knee(serial: "1311S",name: "Weight Activated Brake Knee",pdacReq: 1,kLev: 1,weightLim: 275.0,buildHeight: 48.0),
+    
+    Knee(serial: "1313A",name: "Manual Locking Knee - Type A",pdacReq: 1,kLev: 1,weightLim: 198.0,buildHeight: 43.0),
+    Knee(serial: "1313A-N",name: "Manual Locking Knee - Type B",pdacReq: 1,kLev: 1,weightLim: 220.0,buildHeight: 43.5),
+    
+    Knee(serial: "1312S",name: "Mechanical 4-Bar Knee",pdacReq: 0,kLev: 1,weightLim: 275.0,buildHeight: 83.0),
+    //Knee(serial: "1314",name: "4-Bar Knee with Extension Assist for KD",pdacReq: 1,kLev: 3,weightLim: 275.0,buildHeight: 111.45),
+    //Knee(serial: "1315",name: "4-Bar Knee with Extension Assist for KD",pdacReq: 1,kLev: 3,weightLim: 275.0,buildHeight: 111.45),
+    //Knee(serial: "1317",name: "Single Axis Knee with Internal Extension Assist and Adjustable Constant Friction",pdacReq: 1,kLev: 3,weightLim: 275.0,buildHeight: 77.32),
+    Knee(serial: "1318S",name: "Single Axis Knee with Manual Lock",pdacReq: 1,kLev: 1,weightLim: 275.0,buildHeight: 82.0),
 ]
 
 class KneeListViewController: UITableViewController {
@@ -64,7 +85,6 @@ class KneeListViewController: UITableViewController {
         return cell
     }
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         let filteredArray = kneeList.filter { $0.pdacReq == searchParam.pdacVariable && $0.kLev == searchParam.kLevel && $0.weightLim >= searchParam.weight}
